ab8af518dcc218e024e7a51fe89c83bec28de0a8
dinhman1994 <binhbg14@gmail.com>
2022-03-26 18:05:42 +0700

    init

--- App.js
+++ App.js
@@ -1,10 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableWithoutFeedback,
+  TouchableHighlight,
+  Button,
+  Dimensions,
+} from "react-native";
+import {
+  useDimensions,
+  useDeviceOrientation,
+} from "@react-native-community/hooks";
 
+import { io } from "socket.io-client";
 export default function App() {
+  const { landscape } = useDeviceOrientation();
+  let x = 1;
+
+  //** Socket Config */
+  let socket = io("http://localhost:5000", {
+    transports: ["websocket"],
+    forceNew: true,
+    path: "/test/",
+  });
+
+  socket.on("connection", () => console.log("Connection socket"));
+  socket.on("connect_error", (error) => {
+    console.log(`${error}`);
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View
+        style={{
+          backgroundColor: "red",
+          width: landscape ? "50%" : "10%",
+          height: 50,
+        }}
+      ></View>
+      {/* <Button 
+        title='Click me' 
+        onPress={() => console.log("Btn pressed") } 
+      /> */}
+      {/* <TouchableHighlight
+        onPress={ () => console.log("Ok") }
+      >
+        <Image
+        blurRadius={5}
+        fadeDuration={1000}
+        source={{
+          width: 200,
+          height: 300, 
+          uri: 'https://picsum.photos/200/300' }} 
+        />
+      </TouchableHighlight> */}
+      <Text>
+        Open up App.js to start working on your app ! This is the first time I
+        try to make an adroidddd{" "}
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +69,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
